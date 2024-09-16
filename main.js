0bc44caf72727f2ad938454b98608de63e353164
argconseil <ceo@argconseil.com>
2024-09-16 15:05:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
+const http = require('http');
 const WebSocket = require('ws');
 
+// Créer un serveur HTTP de base
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Serveur WebSocket en écoute\n');
+});
+
+// Utilisation du port attribué par Heroku
+const port = process.env.PORT || 8080;
+
 // Création du serveur WebSocket
-const wss = new WebSocket.Server({ port: 5001 });
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   console.log('Client connecté');
 
-  // Lorsque le serveur reçoit un message du client
   ws.on('message', function incoming(message) {
     console.log('Message reçu:', message);
 
-    // Exemple de commande pour déconnecter un client
     if (message === 'disconnect') {
-      ws.close(1000, 'Déconnexion demandée'); // Code 1000 : déconnexion normale
+      ws.close(1000, 'Déconnexion demandée');
     }
   });
 
-  // Lorsque la connexion est fermée
   ws.on('close', function close(code, reason) {
     console.log(`Connexion fermée. Code: ${code}, Raison: ${reason}`);
   });
 });
 
-  console.log('Serveur WebSocket en écoute sur le port 8080');
-
+// Démarrage du serveur HTTP sur le port attribué
+server.listen(port, () => {
+  console.log(`Serveur WebSocket en écoute sur le port ${port}`);
+});
